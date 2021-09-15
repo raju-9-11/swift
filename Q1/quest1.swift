59a4c353ca3e2966111103420c39fd76c9755ffd
Raju <raju2000119@gmail.com>
2021-09-15 17:51:33 +0530

    Question 1

---
+++ Q1/quest1.swift
@@ -0,0 +1,34 @@
+func leapYear(_ year: Int) -> Bool {
+    if year%4 == 0 {
+        if year%100 == 0 {
+            if year%400 == 0 {
+                return true
+            }
+            return false
+        }
+        return true
+    }
+    return false
+}
+
+func countMinutes(_ month: Int, _ year: Int) -> Int {
+    let minInDay: Int = 1440
+    if month == 2 {
+        if leapYear(year) {
+            return minInDay * 29
+        }
+        return minInDay * 28
+    }
+    let month = month > 7 ? month%7 : month
+    return minInDay * (month % 2 == 0 ? 30 : 31)
+    
+}
+var myMinutes:[Int:[Int]] = [:]
+for year in 2008...2021 {
+    var minutes: [Int] = []
+    for month in 1...12 {
+        minutes.append(countMinutes(month,year))
+    }
+    myMinutes[year] = minutes
+}
+print(myMinutes)
