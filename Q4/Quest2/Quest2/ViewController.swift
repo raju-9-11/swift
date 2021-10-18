c5cb3d48081817a02151debbb9df72d34685a806
Rajkumar S <raju2000119@gmail.com>
2021-10-18 10:22:01 +0530

    Quest 3

--- Q4/Quest2/Quest2/ViewController.swift
+++ Q4/Quest2/Quest2/ViewController.swift
@@ -8,10 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var myTextField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onTextEnter() {
+        let alert = UIAlertController(title: "String Entered", message: myTextField.text ?? "Unknown" , preferredStyle: .alert)
+        let action = UIAlertAction(title: "Okay", style: .cancel, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
     }
 
 
--- Q4/Quest3/Quest3/ViewController.swift
+++ Q4/Quest3/Quest3/ViewController.swift
@@ -9,22 +9,55 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var purpleButton: UIButton!
-    @IBOutlet weak var yellowButton: UIButton!
+    var purpleButton: UIButton!
+    var yellowButton: UIButton!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    
+    override func loadView() {
+        
+        view = UIView()
+        view.backgroundColor = .white
+        
+//      Purple button initialization
+        purpleButton = UIButton(type: .system)
+        purpleButton.setTitle("Purple Button", for: .normal)
         purpleButton.backgroundColor = .purple
+        purpleButton.translatesAutoresizingMaskIntoConstraints = false
+        purpleButton.setTitleColor(.black, for: .normal)
+        purpleButton.tintColor = .black
+        view.addSubview(purpleButton)
+        
+//      Yellow Button Initialization
+        yellowButton = UIButton(type: .system)
+        yellowButton.setTitle("Yellow Button", for: .normal)
+        yellowButton.setTitleColor(.black, for: .normal)
+        yellowButton.translatesAutoresizingMaskIntoConstraints = false
         yellowButton.backgroundColor = .yellow
-        purpleButton.tintColor = .white
         yellowButton.tintColor = .black
+        view.addSubview(yellowButton)
+        
+        NSLayoutConstraint.activate([
+            purpleButton.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor),
+            purpleButton.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor),
+            purpleButton.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor),
+            yellowButton.bottomAnchor.constraint(equalTo: view.layoutMarginsGuide.bottomAnchor),
+            yellowButton.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor),
+            yellowButton.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor),
+        
+        ])
+        
+        purpleButton.addTarget(self, action: #selector(onPurpleClick), for: .touchUpInside)
+        yellowButton.addTarget(self, action: #selector(onYellowClick), for: .touchUpInside)
+        
     }
     
-    @IBAction func onPurpleClick() {
+    @objc
+    func onPurpleClick() {
         view.backgroundColor = .purple
     }
     
-    @IBAction func onYellowClick() {
+    @objc
+    func onYellowClick() {
         view.backgroundColor = .yellow
     }
 
