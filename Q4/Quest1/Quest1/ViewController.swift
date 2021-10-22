51ccceae5c38d380ccf840080409e77c6a14a19f
Rajkumar S <raju2000119@gmail.com>
2021-10-22 15:04:37 +0530

    Q4/Quest1

--- Q4/Quest1/Quest1/ViewController.swift
+++ Q4/Quest1/Quest1/ViewController.swift
@@ -9,17 +9,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var mySwitch: UISwitch!
+    var mySwitch: UISwitch!
+    var myLabel: UILabel!
     
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        onToggle()
+    override func loadView() {
+        view = UIView()
+        view.backgroundColor = .yellow
+        
+        //Slider initialization
+        mySwitch = UISwitch()
+        mySwitch.translatesAutoresizingMaskIntoConstraints = false
+        mySwitch.addTarget(self, action: #selector(onToggle), for: .allEvents)
+        mySwitch.isOn = false
+        view.addSubview(mySwitch)
+        
+        //Label initialization
+        myLabel = UILabel()
+        myLabel.text = " Change Background "
+        myLabel.translatesAutoresizingMaskIntoConstraints = false
+        myLabel.textAlignment = .center
+        myLabel.font = .italicSystemFont(ofSize: 14)
+        myLabel.textColor = .red
+        view.addSubview(myLabel)
+        
+        
+        
+        NSLayoutConstraint.activate([
+            mySwitch.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            mySwitch.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            myLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            myLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 50)
+        
+        ])
         
     }
     
-    @IBAction func onToggle() {
+    @objc
+    func onToggle(_ sender: UISwitch) {
         if(mySwitch.isOn) {
             view.backgroundColor = .purple
         } else {
